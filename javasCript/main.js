a8809c12f82f15c8cd267a1b4eced13521cf4b6e
ashley10 <axagudelo4@misena.edu.co>
2021-03-22 21:24:59 -0500

    recuerda precio con c no con s jajajaja
    ya tu sabe

--- javasCript/main.js
+++ javasCript/main.js
@@ -5,6 +5,17 @@ const StarSesion = document.getElementById('btn-iniciar-sesion')
  */
 
 
+const boton = document.getElementById("boton")
+const catalogo = document.getElementById("contenedor2")
+const texto = document.getElementById("texto")
+  
+  
+boton.addEventListener("click", (e) => {
+  e.preventDefault()
+  catalogo.style.visibility= "visible"
+  texto.style.visibility="hidden"
+})
+
 
 
 
